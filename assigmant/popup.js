6c6997087307667c53181d46e9d244fc7c563fdf
GioGio2004 <116484981+GioGio2004@users.noreply.github.com>
2024-03-02 13:32:21 +0400

    Add files via upload

---
+++ assigmant/popup.js
@@ -0,0 +1,7 @@
+document.getElementById('display-button').addEventListener('click', function() {
+    const inputText = document.getElementById('input-text').value;
+    document.getElementById('output-text').textContent = inputText;
+  });
+  
+
+  
